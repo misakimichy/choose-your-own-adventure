5541d9d7fa9429fb6ed8c56279d1c3146e8f965d
Student <example@example.com>
2019-09-26 16:57:59 -0700

    Delete attempts.

--- Models/ChooseYoursScenario.cs
+++ Models/ChooseYoursScenario.cs
@@ -6,13 +6,11 @@ namespace ChooseYourScenario.Models
     {
         public string UserName { get; set; }
         public string CatName { get; set; }
-        public int TotalAttempts { get; set;}
 
         public Game ()
         {
             UserName = "";
             CatName = "";
-            TotalAttempts = 0;
         }
     }
 }
